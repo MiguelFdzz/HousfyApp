3158db65c883b8871f4e875b84dc2c72d23db848
MiguelFdzz <mifeva1151@gmail.com>
2025-11-13 20:41:41 +0100

    + Menu items, pagina principal items

--- app/src/main/java/fdz/migue/housfyapp/MainActivity.kt
+++ app/src/main/java/fdz/migue/housfyapp/MainActivity.kt
@@ -7,15 +7,32 @@ import android.view.MenuItem
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -33,10 +50,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import fdz.migue.housfyapp.ui.theme.HousfyAppTheme
 import kotlinx.coroutines.launch
 
@@ -49,6 +73,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
 
 @Composable
 fun PantallaPrincipal(modifier: Modifier = Modifier){
@@ -90,7 +116,18 @@ fun DrawerContent(modifier: Modifier = Modifier){
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier){
-
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item{
+            RoundedBackground{
+                Text("¡Bienvenid@ a Housfy, Maria José!", fontSize = 25.sp, textAlign = TextAlign.Center)
+            }
+        }
+    }
 }
 
 @Composable
@@ -120,31 +157,164 @@ fun TopBar(
 }
 
 @Composable
-fun ProfileContent(modifier: Modifier = Modifier) {
+fun ProfileContent(
+    modifier: Modifier = Modifier,
+    photoUrl: String? = null
+) {
     Row(
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
+        Box(
+            modifier = modifier
+                .size(56.dp)
+                .align(Alignment.Bottom)
+        ) {
+            if (photoUrl.isNullOrEmpty()){
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Perfil",
+                    modifier = Modifier
+                        .padding(end = 8.dp)
+                        .fillMaxSize()
+                )
+            } else {
+                AsyncImage(
+                    model = photoUrl,
+                    contentDescription = "Foto de perfil",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(CircleShape)
+                )
+            }
+            Box(
+                modifier = modifier
+                    .size(19.dp)
+                    .offset(x = 30.dp, y = 30.dp)
+                    .padding(2.dp)
+                    .background(
+                        color = Color.Green,
+                        shape = CircleShape
+                    )
+            )
+        }
+        Text(text = "Maria José", fontSize = 17.sp)
+
+        Spacer(modifier = Modifier.weight(1f))
+
         Icon(
-            imageVector = Icons.Default.AccountCircle,
-            contentDescription = "Perfil",
+            imageVector = Icons.Default.Edit,
+            contentDescription = "Editar perfil",
             modifier = Modifier
-                .padding(end = 8.dp)
-                .size(48.dp)
+                .size(30.dp)
+                .padding(end=8.dp)
+                .clickable(
+                    onClick = {}
+                )
         )
-        Text(text = "Miguel", fontSize = 17.sp)
     }
 }
 
 @Composable
 fun MenuItemsContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+    ) {
+        MenuItem(
+            icon = Icons.Default.Home,
+            text = "Página de inicio"
+        )
+        MenuItem(
+            icon = Icons.Default.AddCircle,
+            text = "Tareas"
+        )
+        MenuItem(
+            icon = Icons.Default.DateRange,
+            text = "Actividades"
+        )
+        MenuItem(
+            icon = Icons.Default.ShoppingCart,
+            text = "Lista de la compra"
+        )
+        MenuItem(
+            icon = Icons.Default.MailOutline,
+            text = "Chat Grupal"
+        )
 
+        Spacer(modifier = Modifier.weight(1f))
+        HorizontalDivider()
+
+        MenuItem(
+            icon = Icons.Default.Settings,
+            text = "Configuración"
+        )
+    }
 }
 
 @Preview
 @Composable
 fun Preview(){
     PantallaPrincipal()
+}
+
+@Composable
+fun MenuItem(
+    icon: ImageVector,
+    text: String,
+    onClick: () -> Unit = {}
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .clip(RoundedCornerShape(12.dp)) // esquinas redondeadas
+            .background(MaterialTheme.colorScheme.surfaceVariant) // fondo
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.surfaceVariant,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .clickable { onClick() }
+            .padding(12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier.size(24.dp),
+            tint = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 12.dp)
+        )
+    }
+}
+
+@Composable
+fun RoundedBackground(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = MaterialTheme.colorScheme.surfaceVariant,
+    borderColor: Color = MaterialTheme.colorScheme.surfaceVariant,
+    cornerRadius: Dp = 12.dp,
+    content: @Composable () -> Unit // aquí se pasa cualquier contenido
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(cornerRadius))
+            .background(backgroundColor)
+            .border(
+                width = 1.dp,
+                color = borderColor,
+                shape = RoundedCornerShape(cornerRadius)
+            )
+            .padding(12.dp) // padding interno opcional
+    ) {
+        content() // aquí se renderiza lo que le pases
+    }
 }
